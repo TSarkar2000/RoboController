4e70822208ed1f1249e954f762707a9b74de1053
Tanmoy Sarkar <46512545+TSarkar2000@users.noreply.github.com>
2021-03-08 17:32:40 +0530

    Added "app2" module to use MQTT while "app" will use the plain old direct TCP connection.

---
+++ app2/src/main/java/com/tsc/robocontrollermqtt/utils/Prefs.java
@@ -0,0 +1,14 @@
+package com.tsc.robocontrollermqtt.utils;
+
+public class Prefs {
+
+    public static final String KEY_IP = "ip";
+    public static final String KEY_ROTATIONS = "rSteps";
+    public static final String KEY_HOST = "mqttBroker";
+
+    public static final String DEF_IP = "192.168.43.43";
+    public static final int DEF_ROTATIONS = 20;
+
+    public static final String DEF_HOST = "http://broker.mqtt-dashboard.com";
+
+}
